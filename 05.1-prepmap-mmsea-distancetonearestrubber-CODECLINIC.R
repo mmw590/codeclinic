f9393a63aebfe7933780afa0bc53d977960d06b3
Maria Wang Mei Hua <10426014+mmw590@users.noreply.github.com>
2020-02-19 14:38:42 +0000

    better script
    remove unneccessary code and corrected input file

--- 05.1-prepmap-mmsea-distancetonearestrubber-CODECLINIC.R
+++ 05.1-prepmap-mmsea-distancetonearestrubber-CODECLINIC.R
@@ -16,24 +16,14 @@ rm(list=ls())
 memory.limit(300000)
 
 ######------------------ Load packages ----------------------#######
-library(tidyverse)
 library(raster)
 
 ######------------------ Dataset and management ----------------------#######
 
-# Set crs ####
-crs_wgs <- CRS('+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0')
-
 #### Hurni MMSEA Map ---- #####
-mmsea_allyears <- stack("mmsea_allyears_wgs.tif")
-
-#### Reclassify to make a binary raster with only plantations
-lcc <- unique(values(mmsea_allyears[[1]]))
-reclassify.df <- data.frame(oldval=9, newval=1) %>% arrange(oldval)
-system.time(mmsea2000 <- subs(mmsea_allyears[[1]], reclassify.df, by='oldval', which='newval',subsWithNA=TRUE)) #76s
-unique(values(mmsea2000))
+mmsea2000 <- raster('mmsea2000_wgs_rubberonly_usethis.tif')
 plot(mmsea2000)
 
 system.time(distance(mmsea2000, filename='mmsea2000_wgs_disttonearestrubber2.tif')) #takes a long time (maybe a week?) #maybe resolution too fine
 
-mmsea_distrub <- raster(paste0(GISfolder, 'Jeff Fox/byyear/mmsea2000_wgs_disttonearestrubber2.tif'))
+#mmsea_distrub <- raster(paste0(GISfolder, 'Jeff Fox/byyear/mmsea2000_wgs_disttonearestrubber2.tif'))
